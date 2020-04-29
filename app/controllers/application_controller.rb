755bf5d1b67a52cb7bb6944db932c2156e3b0664
Lawrence Chou <lawrencechou1024@gmail.com>
2020-04-29 10:56:23 +0800

    fix(<footer>): 在 manuel connection switching 的 block 裡無法用 AdminRecord

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < ActionController::Base
 
   def switch_tenant_shard_database
     if request.subdomain.present?
+      # TODO: 有沒有辦法在 L12 裡還能使用 TenantConfig 呀？
+      @tenant_config = TenantConfig.find_by_subdomain(request.subdomain)
+
       ActiveRecord::Base.connected_to(shard: request.subdomain.to_sym) do
         yield
       end
